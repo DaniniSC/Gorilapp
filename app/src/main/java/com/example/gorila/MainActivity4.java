bcebe938b57fd4cbc44856d75842f88b28a0d4d4
Daniela Santander <dpsantandercastro@gmail.com>
2020-06-20 17:55:32 -0400

    Feat: add java files

---
+++ app/src/main/java/com/example/gorila/MainActivity4.java
@@ -0,0 +1,14 @@
+package com.example.gorila;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+// Diagrama de clases - Mati
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
---
+++ app/src/main/java/com/example/gorila/MainActivity7.java
@@ -0,0 +1,14 @@
+package com.example.gorila;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+// IMC
+public class MainActivity7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main7);
+    }
+}
