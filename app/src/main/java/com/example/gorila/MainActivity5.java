abfd2e827d357def01637e1ba20a2f9c08c58b5a
MatiasPineda <matias.pinedav@gmail.com>
2020-06-21 00:26:42 -0400

    'Fix: main_activity5.xml'

--- app/src/main/java/com/example/gorila/MainActivity5.java
+++ app/src/main/java/com/example/gorila/MainActivity5.java
@@ -3,19 +3,55 @@ package com.example.gorila;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // Cálculo relacionado
-public class MainActivity5 extends AppCompatActivity {
+public class MainActivity5 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner opciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
-    }
 
+
+        Spinner opciones = (Spinner) findViewById(R.id.spinner);
+        opciones.setOnItemSelectedListener(this);
+
+        Button button = (Button)findViewById(R.id.btn_dcto);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.opciones, android.R.layout.simple_spinner_item);        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+    }
     public void IrMenu(View view) {
         Intent obj_intent = new Intent(this, MainActivity.class);
         startActivity(obj_intent);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
