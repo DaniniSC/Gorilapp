748b72893e006b14732827079dec9c6891800f1f
MatiasPineda <matias.pinedav@gmail.com>
2020-06-23 21:08:58 -0400

    Fix: Class Persona, MainActivity7

--- app/src/main/java/com/example/gorila/MainActivity7.java
+++ app/src/main/java/com/example/gorila/MainActivity7.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 // IMC
 public class MainActivity7 extends AppCompatActivity {
+    Persona persona = new Persona();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,28 +18,19 @@ public class MainActivity7 extends AppCompatActivity {
     }
 
     public void CalcularIMC(View view) {
-        TextView imc = (TextView)findViewById(R.id.imc);
+        TextView imc = findViewById(R.id.imc);
         EditText altura = findViewById(R.id.altura);
         EditText peso = findViewById(R.id.peso);
 
         double calc_peso = Double.parseDouble(peso.getText().toString());
         double calc_altura = Double.parseDouble(altura.getText().toString()) / 100;
 
-        double calc_imc = calc_peso / (calc_altura * calc_altura);
+        persona.setPeso(calc_peso);
+        persona.setAltura(calc_altura);
 
-        String nivel_imc = "";
+        String msj = persona.ClasificarIMC();
 
-        if (calc_imc < 18.5) {
-            nivel_imc = "BAJO PESO";
-        } else if (calc_imc >= 18.5 && calc_imc <= 25) {
-            nivel_imc = "NORMAL";
-        } else if (calc_imc > 25 && calc_imc < 30) {
-            nivel_imc = "SOBREPESO";
-        } else {
-            nivel_imc = "OBESIDAD";
-        }
-
-        imc.setText("Tu IMC es: " + calc_imc + ", por tanto tu nivel indica " + nivel_imc);
+        imc.setText(msj);
     }
 
     public void IrMenu(View view) {
--- app/src/main/java/com/example/gorila/Persona.java
+++ app/src/main/java/com/example/gorila/Persona.java
@@ -1,16 +1,17 @@
 package com.example.gorila;
 
 public class Persona {
+
     private int edad;
     private char sexo;
     private double peso;
-    private int altura;
+    private double altura;
 
     public Persona() {
-        edad = 0;
-        sexo = 'N';
-        peso = 0.0;
-        altura = 0;
+        this.edad = 0;
+        this.sexo = 'N';
+        this.peso = 0.0;
+        this.altura = 0;
     }
 
     public Persona(int edad, char sexo, double peso, int altura) {
@@ -18,6 +19,59 @@ public class Persona {
         this.sexo = sexo;
         this.peso = peso;
         this.altura = altura;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+
+    public String ClasificarIMC(){
+
+        double calc_imc = this.peso / (this.altura * this.altura);
+
+        String nivel_imc = "";
+
+        if (calc_imc < 18.5) {
+            nivel_imc = "BAJO PESO";
+        } else if (calc_imc >= 18.5 && calc_imc <= 25) {
+            nivel_imc = "NORMAL";
+        } else if (calc_imc > 25 && calc_imc < 30) {
+            nivel_imc = "SOBREPESO";
+        } else {
+            nivel_imc = "OBESIDAD";
+        }
+
+        return "Tu IMC es: " + String.format("%.2f", calc_imc) + ", por tanto tu nivel indica " + nivel_imc;
+
     }
 
 
